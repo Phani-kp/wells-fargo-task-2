9b20c871903e035aeb78561344c1d126ef7c2338
Phani-kp <Phaneendhrak0427@gmail.com>
2025-01-23 20:05:58 -0500

    first commit

---
+++ src/main/java/com/wellsfargo/counselor/entity/customer.java
@@ -0,0 +1,48 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Default constructor required by JPA
+    public customer() {
+    }
+
+    // Constructor to initialize all instance variables
+    public customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
